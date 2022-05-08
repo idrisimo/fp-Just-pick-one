796c84583c549e5ede357938a291a4ee2873e668
idrisimo <idrissilva@hotmail.com>
2022-05-08 16:22:40 +0100

    tmdb view now returns JSON data

--- api/tmdb_api_app/views.py
+++ api/tmdb_api_app/views.py
@@ -1,35 +1,42 @@
+from urllib import request
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 import requests
 import environ
 import os
+from rest_framework.response import Response
 # Initialise environmental variables
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, 'api/.env'))
 
-def get_movie_data(req, choices):
-    
-    options = {
-        'genre':'&with_genres=',
-        'adult':'&include_adult=',
-        'language':'&language=',
-        'release_date_start':'&primary_release_date.gte=',
-        'release_data_end':'&primary_release_date.lte=',
-        'watch_providers':'&with_watch_providers=',
-        'region':'&region=',
-        'pages':'&page='
-    }
-    query_string = ''
-    for key, value in choices.items():
-        if value != '':
-            query_string = f'{query_string}{options[key]}{value}'
-        
 
-    try: 
+def get_movie_data(req):
+    options = {
+        'genre': '&with_genres=',
+        'adult': '&include_adult=',
+        'language': '&language=',
+        'release_date_start': '&primary_release_date.gte=',
+        'release_data_end': '&primary_release_date.lte=',
+        'watch_providers': '&with_watch_providers=',
+        'region': '&region=',
+        'pages': '&page='
+    }
+
+    if req.method == 'POST':
+        choices = req.POST.dict()
+
+        query_string = ''
+        
+        for key, value in choices.items():
+            if value != '':
+                query_string = f'{query_string}{options[key]}{value}'
+                
         base_url = 'https://api.themoviedb.org/3/discover/movie?'
         api_key = f"api_key={env('TMDB_KEY')}"
-        response = requests.get(f'{base_url}{api_key}{query_string}')
+        final_url = f'{base_url}{api_key}{query_string}'
+        response = requests.get(final_url)
         data = response.json()
-        return HttpResponse(data['results'])
-    except Exception as err:
-        return HttpResponse(err)
+        return JsonResponse(data)
+    else:
+        return HttpResponse('No Access via this route') # TODO Deal with this better. 
