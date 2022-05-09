54ac138cdbd9b6c3f4455f3edbc0750cb72e9447
HevvsOlivia <heather@mcglynn.me.uk>
2022-05-09 17:40:22 +0100

    adding join room feature to account page

--- client/src/pages/UserAccount/index.js
+++ client/src/pages/UserAccount/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavButton, LogoutButton } from "../../components";
+import { NavButton, LogoutButton, JoinRoomFeature } from "../../components";
 
 export function UserAccount() {
 
@@ -8,6 +8,7 @@ export function UserAccount() {
         <NavButton id="newBtn" value="Start new Game" path="/StreamingService"/>
         <NavButton id="prefBtn" value="Use previous preferences" path="/UsedPreferences"/>
         <NavButton id="editBtn" value="Edit previous preferences" path="/EditPreferences"/>
+        <JoinRoomFeature />
         <NavButton id="HowItWorksBtn" value="How it Works" path="/HowItWorks"/>
         <LogoutButton/>
         </>
