aae6af946f65e8e68960748485464eaf72eb8813
HevvsOlivia <heather@mcglynn.me.uk>
2022-05-09 17:42:26 +0100

    waiting room

--- client/src/pages/WaitingRoom/index.js
+++ client/src/pages/WaitingRoom/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { BackButton } from "../../components";
 
 export function WaitingRoom() {
 
     return(
         <>
+        
+        
+        <BackButton />
         </>
     )
 }
