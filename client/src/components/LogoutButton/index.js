ef0afcb1ef4a55e980abad88039733b55a74cd60
HevvsOlivia <heather@mcglynn.me.uk>
2022-05-09 15:19:18 +0100

    logout button created

---
+++ client/src/components/LogoutButton/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export function LogoutButton() {
+
+    const navigateTo = useNavigate();
+
+    const logoutFunction = () => {
+        localStorage.clear()
+    }
+
+    const handleLogout = async () => {
+        await logoutFunction();
+        navigateTo('/')
+    }
+
+    return(
+        <input type="submit" id="logoutBtn" value="Logout" onClick={handleLogout}/>
+       
+    )
+}
