d49d0b4f0116ef28e44527e9bdbce0be734bf3c9
HevvsOlivia <heather@mcglynn.me.uk>
2022-05-09 17:19:32 +0100

    basic usedpref page setup

--- client/src/pages/UsedPreferences/index.js
+++ client/src/pages/UsedPreferences/index.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import { BackButton } from "../../components";
 
 export function UsedPreferences() {
 
+    const [players, setPlayers] = useState("2");
+
     return (
         <>
+        <form id="UsedPrefForm">
+            <label htmlFor="numPeople">Number of people:</label>
+            <input type="number" name="numPeople" min="2" max="10" value={players || "2"}/>
+            <input type="submit" value="START" style={{cursor: 'pointer'}}/>          
+
+        </form>
+        <BackButton />
         </>
     )
 }
