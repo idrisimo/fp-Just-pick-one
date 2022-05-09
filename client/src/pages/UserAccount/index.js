0ec4a56ac4593eba73d490d27e962e035a818514
HevvsOlivia <heather@mcglynn.me.uk>
2022-05-09 15:20:01 +0100

    basic account page

--- client/src/pages/UserAccount/index.js
+++ client/src/pages/UserAccount/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { NavButton, LogoutButton } from "../../components";
 
 export function UserAccount() {
 
     return(
         <>
+        <NavButton id="newBtn" value="Start new Game" path="/StreamingService"/>
+        <NavButton id="prefBtn" value="Use previous preferences" path="/UsedPreferences"/>
+        <NavButton id="editBtn" value="Edit previous preferences" path="/EditPreferences"/>
+        <NavButton id="HowItWorksBtn" value="How it Works" path="/HowItWorks"/>
+        <LogoutButton/>
         </>
     )
 }
