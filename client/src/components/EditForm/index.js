594247d089ef2a4ac6ca4e9e5308e0b1925d6ea5
isabel <isargp@gmail.com>
2022-05-11 11:33:51 +0100

    Trying multiple select

--- client/src/components/EditForm/index.js
+++ client/src/components/EditForm/index.js
@@ -16,6 +16,8 @@ export function EditForm() {
       "country": null,
       "platform": null
    });
+   const genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
+
 
    useEffect( () => {
       const getPref = async () => {
@@ -50,6 +52,7 @@ export function EditForm() {
       }
    }
 
+
    const renderUserPreferences = () => {
 
       const genre = userPreferences.genre ? userPreferences.genre : "Any"
@@ -70,6 +73,13 @@ export function EditForm() {
       )
    }
 
+   const renderGenreOptions = () => {
+      return genres.map(genre => <><input type="checkbox" aria-label="Genre" name="genre" value={genre.id} onChange={updateInput}/>{genre.name}<br/></>)
+   }
+
+   
+
+
    return(
       <>
 
@@ -77,8 +87,15 @@ export function EditForm() {
 
       <form aria-label="editPreferencesForm" id="edit-preferences-form" onSubmit={handleSubmit}>
 
-         <label htmlFor="Genre">Genre: </label>
-         <input type="text" aria-label="Genre" name="genre" onChange={updateInput}/>
+         <label htmlFor="Genre">Genre: </label><br/>
+         {renderGenreOptions()}
+         
+         {/* <select name="genre" multiple={true}>
+            <option value="28">Action</option>
+            <option value="12">Adventure</option>
+            <option value="16">Animation</option>
+            <option value="35">Comedy</option>
+         </select> */}
 
          <label htmlFor="Year">Year: </label>
          <input type="number" aria-label="Year" name="year" onChange={updateInput}/>
@@ -91,11 +108,11 @@ export function EditForm() {
 
          <input type="submit" className="submitBtn" value="Save" style={{cursor: 'pointer'}}/>
 
-
       </form>
+
       {loading && (
          <div id="loading">
-            Registering Account . . .
+            Loading Preferences . . .
          </div>
       )}
 
