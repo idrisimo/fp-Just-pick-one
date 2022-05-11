c688dc656b1a9248958ee0257e87d6e4a6b935a9
isabel <isargp@gmail.com>
2022-05-11 14:13:25 +0100

    Preferences form: countries are also working

--- client/src/components/EditForm/index.js
+++ client/src/components/EditForm/index.js
@@ -16,7 +16,10 @@ export function EditForm() {
       "country": null,
       "platform": null
    });
+
    const genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
+
+   const countries= [{"value": null, "name": "--No Country Preferences--"},{"value": "AL", "Albania": "Spain"},{"value": "AT", "name": "Austria"},{"value": "BY", "name": "Belarus"},{"value": "BE", "name": "Belgium"},{"value": "BA", "name": "Bosnia and Herzegovina"},{"value": "BG", "name": "Bulgaria"},{"value": "HR", "name": "Croatia"},{"value": "CY", "name": "Cyprus"},{"value": "CZ", "name": "Czech Republic"},{"value": "DK", "name": "Denmark"},{"value": "EE", "name": "Estonia"},{"value": "FI", "name": "Finland"},{"value": "FR", "name": "France"},{"value": "DE", "name": "Germany"},{"value": "GI", "name": "Gibraltar"},{"value": "GR", "name": "Greece"},{"value": "HU", "name": "Hungary"},{"value": "IS", "name": "Iceland"},{"value": "IE", "name": "Ireland"},{"value": "IT", "name": "Italy"},{"value": "LV", "name": "Latvia"},{"value": "LT", "name": "Lithuania"},{"value": "LU", "name": "Luxembourg"},{"value": "MK", "name": "Macedonia"},{"value": "MT", "name": "Malta"},{"value": "MD", "name": "Moldova"},{"value": "MC", "name": "Monaco"},{"value": "ME", "name": "Montenegro"},{"value": "NL", "name": "Netherlands"},{"value": "NO", "name": "Norway"},{"value": "PL", "name": "Poland"},{"value": "PT", "name": "Portugal"},{"value": "RO", "name": "Romania"},{"value": "RS", "name": "Serbia"},{"value": "SK", "name": "Slovakia"},{"value": "SI", "name": "Slovenia"},{"value": "ES", "name": "Spain"},{"value": "SE", "name": "Sweden"},{"value": "CH", "name": "Switzerland"},{"value": "UA", "name": "Ukraine"},{"value": "GB", "name": "United Kingdom"},{"value": "RS", "name": "Yugoslavia"}]
 
 
    useEffect( () => {
@@ -41,7 +44,14 @@ export function EditForm() {
       try{
          setLoading(true);
 
-         const newPreferences = await createPreferences(formInput)
+         const data = {
+            "genre":formInput.genre,
+            "year":formInput.year,
+            "country": e.target['country'].value,
+            "platform": formInput.platform
+         }
+
+         const newPreferences = await createPreferences(data)
          console.log(newPreferences)
          if (newPreferences === "Successful"){
             navigateTo('/UserAccount')
@@ -56,7 +66,7 @@ export function EditForm() {
 
    const renderUserPreferences = () => {
       const genre = userPreferences.genre ? userPreferences.genre.split(",") : "Any"
-      const country = userPreferences.country ? userPreferences.country : "Any"
+      let country = userPreferences.country ? userPreferences.country : "Any"
       const year = userPreferences.year ? userPreferences.year : "Any"
       const platform = userPreferences.platform ? userPreferences.platform : "Any"
       let genreNames = ""
@@ -68,6 +78,10 @@ export function EditForm() {
          genreNames = genreNames.slice(0, -1)
       } else {
          genreNames = genre
+      }
+
+      if(country!="Any"){
+         country = countries.find( c => c.value == country).name
       }
 
       return (
@@ -105,6 +119,10 @@ export function EditForm() {
    const renderGenreOptions = () => {
       return genres.map(genre => <><input type="checkbox" aria-label="Genre" name="genre" value={genre.id} onClick={manageMultiGenreSelect}/>{genre.name}<br/></>)
    }
+   
+   const renderCountryOptions = () => {
+      return countries.map( country => <><option value={country.value}>{country.name}</option></>)
+   }
 
    return(
       <>
@@ -120,7 +138,9 @@ export function EditForm() {
          <input type="number" aria-label="Year" name="year" onChange={updateInput}/>
 
          <label htmlFor="Country">Country: </label>
-         <input type="text" aria-label="Country" name="country" onChange={updateInput}/>
+         <select aria-label="Country" name="country">
+            {renderCountryOptions()}
+         </select>
 
          <label htmlFor="Platforms">Platforms: </label>
          <input type="text" aria-label="Platforms" name="platform" onChange={updateInput}/>
