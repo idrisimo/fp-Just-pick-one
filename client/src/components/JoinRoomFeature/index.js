f718054540c5fe7d2dd87dd83783d2c80926c908
isabel <isargp@gmail.com>
2022-05-12 15:46:59 +0100

    before pull

--- client/src/components/JoinRoomFeature/index.js
+++ client/src/components/JoinRoomFeature/index.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./index.css"
 
 export function JoinRoomFeature(handleSubmit, updateInput) {
 
     return (
         <div id="joinForm">
-        <form id="joinRoomForm" onSubmit={handleSubmit}>
-            <label htmlFor="enterCode" id="codeLable"></label>
-            <input type="text" placeholder="enter code here..."required onChange={updateInput}></input>
-            <input type="submit" id="joinRoomBtn" value="Join Room" style={{cursor: "pointer"}}/>
-        </form>
+            <form id="joinRoomForm" onSubmit={handleSubmit}>
+                <label htmlFor="enterCode" id="codeLable"></label>
+                <input type="text" placeholder="enter code here..."required onChange={updateInput}></input><br/>
+                <input type="submit" id="joinRoomBtn" value="Join Room" style={{cursor: "pointer"}}/>
+            </form>
         </div>
     )
 };
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { JoinRoomFeature, NavButton } from '../../components';
+import "./index.css";
 
 export function Home() {
 
@@ -18,12 +19,11 @@ export function Home() {
     // }
 
     return (
-        <>
-        
-        <NavButton id="LoginBtnNav" data-testid="LoginBtnNav" value="Login" path="/Login"/>
-        <NavButton id="RegisterBtnNav" value="Register" path="/Register"/>
-        <NavButton id="HowItWorksBtn" value="How it Works" path="/HowItWorks"/>
-        <JoinRoomFeature handleSubmit={handleSubmit}/> 
-        </>
+        <div id="homepage-buttons-wrapper">
+            <NavButton id="LoginBtnNav" data-testid="LoginBtnNav" value="Login" path="/Login"/>
+            <NavButton id="RegisterBtnNav" value="Register" path="/Register"/>
+            <NavButton id="HowItWorksBtn" value="How it Works" path="/HowItWorks"/><br/>
+            <JoinRoomFeature handleSubmit={handleSubmit}/> 
+        </div>
     )
 }
